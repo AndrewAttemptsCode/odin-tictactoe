c22ccfced1377d6d6ed05e672a9c58f9c828a457
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-06-29 16:15:58 +0100

    Add create game board layout function

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// store the gameboard as an array inside of a Gameboard object
+
+// tuck as much as you can inside factories.
+// single instance of something (e.g. the gameboard,
+// the displayController etc.) then wrap the factory inside
+// an IIFE (module pattern)
+
+const gameBoard = (function() {
+
+    const createBoard = function() {
+        const rows = 3;
+        const columns = 3;
+        const board = [];
+
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < columns; j++) {
+                board[i].push("test");
+            }
+        }
+        return board;
+    };
+
+    return {createBoard};
+})();
+
+const board = gameBoard.createBoard();
+console.log(board);
