aa697967c06ecc03a5b685b64e64965f272b259b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-06-30 15:05:37 +0100

    Add create player module

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,24 @@ const GameBoard = (function() {
 
     return {createBoard, getBoard};
 })();
+
+
+const PlayerModule = (function() {
+
+    const createPlayer = (name, marker) => {
+        return {name, marker};
+    };
+
+    return {createPlayer};
+})();
+
+
+// Test output code
+GameBoard.createBoard();
+console.log(GameBoard.getBoard());
+
+const player1 = PlayerModule.createPlayer("Player 1", "X");
+const player2 = PlayerModule.createPlayer("Player 2", "O");
+
+console.log(player1);
+console.log(player2);
